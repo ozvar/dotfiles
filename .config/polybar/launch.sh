82a80b514bd5d924acc72f641b2dd585ae0fd6ac
ozvar <ozanvardal@gmail.com>
2020-04-08 23:20:31 +0000

    added polybar and rofi to i3 config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar.log
+polybar default >>/tmp/polybar.log 2>&1 &
