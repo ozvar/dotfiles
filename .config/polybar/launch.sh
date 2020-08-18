e786155acb39653ba46823ae729ed93343ad75d9
ozvar <ozanvardal@gmail.com>
2020-08-18 14:46:01 +0000

    new bar name and module order, finished battery and audio module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar.log
-polybar default >>/tmp/polybar.log 2>&1 &
+polybar -c ~/.config/polybar/config main &
