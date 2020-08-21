4bea73dc22a10278ce840e0fd3c2c45ee2379a6e
ozvar <ozanvardal@gmail.com>
2020-08-21 16:55:32 +0300

    neovim and vim-plug

--- software.sh
+++ software.sh
@@ -17,6 +17,7 @@ yay -Syu \
 		python-pip \
 		i3-gaps \
 		rxvt-unicode \
+		neovim \ 
 		ranger \
 		picom \
 		polybar \
@@ -45,4 +46,9 @@ pip3 install jedi \
 		seaborn \
 
 # clone dotfiles somewhere here
+
+
+# install vim-plug to manage neovim plugins
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
